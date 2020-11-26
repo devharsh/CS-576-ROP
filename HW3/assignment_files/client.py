978a5e3af308a67e88b139e159c28fef8d617e5a
Devharsh Trivedi <devharsh@live.in>
2020-11-26 16:47:15 -0500

    changed client.py

--- HW3/assignment_files/client.py
+++ HW3/assignment_files/client.py
@@ -1,7 +1,18 @@
 import socket
+import sys, os
+
+sock = os.getenv("HOME") + "/victim.sock"
 s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-s.connect("/home/dtrived5/victim.sock")
-s.send(b'Hello\n4\n')
+s.connect(sock)
+str = b'A' * 24 + b'\n48\n'
+s.send(str)
 data = s.recv(1024)
 s.close()
-print('Received ' + repr(data))
+
+print(data)
+print(repr(data))
+
+filename = '/tmp/someData.txt'
+with open(filename, 'w') as file_object:
+    file_object.write(repr(data))
+    file_object.write("\n")
